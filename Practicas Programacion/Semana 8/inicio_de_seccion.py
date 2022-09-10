c61d1fc0714a0b8b63fe273103f32500a0f1df34
Adony Ulloa <adonyulloa@gmail.com>
2022-09-10 15:10:41 -0600

    validacion de usuario mas programa que lo utiliza

---
+++ Practicas Programacion/Semana 8/inicio_de_seccion.py
@@ -0,0 +1,9 @@
+
+import modulo_usuario
+
+print("INICIO DE SECCION")
+
+usuario=input("Introduce usuario: ")
+contrasena=input("Introduce contraseña: ")
+
+print(modulo_usuario.validar_nombre_usuario(usuario))
