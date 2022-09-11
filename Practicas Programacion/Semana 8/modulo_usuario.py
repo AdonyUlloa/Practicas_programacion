4a0b2408f07901649e0da8a09351888dd3218af9
Adony Ulloa <adonyulloa@gmail.com>
2022-09-11 00:34:53 -0600

    agrego un white al inicio de sesion y se modifico verificacion usuario

--- Practicas Programacion/Semana 8/modulo_usuario.py
+++ Practicas Programacion/Semana 8/modulo_usuario.py
@@ -1,12 +1,26 @@
 
-def validar_nombre_usuario(nombre):
-    if len(nombre)>=6 and len(nombre)<=12:
-        for i in nombre:
-            if i in 'qwertyuiopasdfghjklñzxcvbnmQWERTYUIOPASDFGHJKLÑZXCVBNM1234567890':
-                return 'Usuario valido'
-            else:
-                return 'El nombre de usuario puede contener solo letras y numeros'
-    elif len(nombre)<6:
-        return 'El nombre de usuario debe contener al menos 6 caracteres'
+'''
+Crear un módulo para validación de nombres de usuarios, que cumplirá las 
+siguientes características: 
+• El nombre de usuario debe contener un mínimo de 6 caracteres y un máximo 
+de 12. 
+• El nombre de usuario debe ser alfanumérico. 
+• Si el nombre de usuario es de menos de 6 caracteres "El nombre de usuario 
+debe contener al menos 6 caracteres". 
+• Nombre de usuario con más de 12 caracteres, retorna el mensaje "El nombre 
+de usuario no puede contener más de 12 caracteres". 
+• Nombre de usuario con caracteres distintos a los alfanuméricos, retorna el 
+mensaje "El nombre de usuario puede contener solo letras y números". 
+• Nombre de usuario válido, retorna “Usuario válido”. 
+'''
+
+def validacion_usuario(usuario):
+    if len(usuario) < 6:
+        return "El nombre de usuario debe contener al menos 6 caracteres"
+    elif len(usuario) > 12:
+        return "El nombre de usuario no puede contener más de 12 caracteres"
+    elif not usuario.isalnum():
+        return "El nombre de usuario puede contener solo letras y números"
     else:
-        return 'El nombre de usuario no puede contener mas de 12 caracteres'
+        return "Usuario válido"
+    
